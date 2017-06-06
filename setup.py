fe9b7f846788c5a83693256620b2d0930d70af06
hedleyroos <hedleyroos@gmail.com>
2017-06-06 21:08:46 +0200

    Prep for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="django-ultracache-twisted",
-    version="0.1",
+    version="0.0.1",
     url="http://github.com/praekelt/django-ultracache-twisted",
     license="BSD",
     description="Companion app to django-ultracache that enables proxy invalidation",
